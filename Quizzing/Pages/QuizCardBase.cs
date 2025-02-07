22a65f4b8696e8324e479f6a609fa9ea90376b97
Taras <elloertly@outlook.com>
2025-02-07 20:30:36 +0200

    in_work

--- Quizzing/Pages/QuizCardBase.cs
+++ Quizzing/Pages/QuizCardBase.cs
@@ -6,6 +6,7 @@ namespace Quizzing.Pages
     public class QuizCardBase : ComponentBase
     {
         public List<Question> Questions { get; set; } = new List<Question>();
+        public List<Question> AnsweredQuestions { get; set; } = new List<Question>();
         protected int questionIndex = 0;
         protected int score = 0;
 
@@ -18,19 +19,28 @@ namespace Quizzing.Pages
 
         protected void OptionSelected(Option option)
         {
+            var currentQuestion = Questions[questionIndex];
+            var selectedOption = currentQuestion.Options.Where(x => x.OptionTitle == option.OptionTitle).First();
             
             if (option.IsRight)
             {
                 score++;
             }
-            option.IsSelected = true;
-           
+            selectedOption.IsSelected = true;
+            currentQuestion.Answer = option.OptionReaction;
+            AnsweredQuestions.Add(currentQuestion);
             questionIndex++;
         }
 
         protected void RestartQuiz()
         {
             score = 0;
+            questionIndex = 0;
+        }
+        
+        protected void ShowAnswers()
+        {
+            Questions = AnsweredQuestions;
             questionIndex = 0;
         }
 
@@ -40,21 +50,44 @@ namespace Quizzing.Pages
             {
                 QuestionTitle = "Який наголос у слові \"разом\"?",
                 Options = new List<Option>() {
-                    new Option { OptionTitle ="разОм", OptionReaction ="Неправильно.Попри те, що багато хто так говорить і навіть співає у піснях" },
-                    new Option { OptionTitle ="рАзом", OptionReaction = "Правильно! Адже рАзом нас багато!" },
-                    new Option { OptionTitle ="Обидва варіанти вірні", OptionReaction = "Неправильно. Вірний варіант лише один - рАзом. Запам'ятай і не помиляйся!" }
-                },
-                Answer = "рАзом"
+                    new Option {
+                        OptionTitle ="разОм", 
+                        OptionReaction ="Неправильно.Попри те, що багато хто так говорить і навіть співає у піснях",
+                        IsRight = false,
+                        IsSelected = false},
+                    new Option { 
+                        OptionTitle ="рАзом", 
+                        OptionReaction = "Правильно! Адже рАзом нас багато!",
+                        IsRight = false,
+                        IsSelected = false
+                    },
+                    new Option { 
+                        OptionTitle ="Обидва варіанти вірні", 
+                        OptionReaction = "Неправильно. Вірний варіант лише один - рАзом. Запам'ятай і не помиляйся!",
+                        IsRight = false,
+                        IsSelected = false}
+                }
             };
             Question q2 = new Question
             {
                 QuestionTitle = "Який наголос у слові \"спина\"?",
                 Options = new List<Option>() {
-                    new Option { OptionTitle ="спИна", OptionReaction ="Маєш рацію! Тільки так - спИна!" },
-                    new Option { OptionTitle ="спинА", OptionReaction = "Ні.Хоча так інколи говорять навіть лікарі, які лікують спИну" },
-                    new Option { OptionTitle ="Обидва варіанти вірні", OptionReaction = "Ні! Правильний наголос лише на \"И\" - спИна!" }
+                    new Option { 
+                        OptionTitle ="спИна", 
+                        OptionReaction ="Маєш рацію! Тільки так - спИна!",
+                        IsRight = false,
+                        IsSelected = false},
+                    new Option {
+                        OptionTitle ="спинА", 
+                        OptionReaction = "Ні.Хоча так інколи говорять навіть лікарі, які лікують спИну",
+                        IsRight = false,
+                        IsSelected = false},
+                    new Option {
+                        OptionTitle ="Обидва варіанти вірні", 
+                        OptionReaction = "Ні! Правильний наголос лише на \"И\" - спИна!",
+                        IsRight = false,
+                        IsSelected = false}
                 },
-                Answer = "спИна"
             };
 
             Questions.AddRange(new List<Question> { q1, q2 });
